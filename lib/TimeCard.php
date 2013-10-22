36361cb2b8b7dfb182c6786db50a351ff6fc972b
makoto sasaki <systemcat91@gmail.com>
2013-10-22 18:01:44 +0900

    add base class.

---
+++ lib/TimeCard.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: makoto
+ * Date: 2013/10/22
+ * Time: 17:58
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace SimpleTimeCard;
+
+
+class TimeCard {
+
+}
