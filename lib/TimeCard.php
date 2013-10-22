55fc1ed2847961e5be3fdb92ad0162dc129964eb
makoto sasaki <systemcat91@gmail.com>
2013-10-23 00:47:33 +0900

    add config file.

--- lib/TimeCard.php
+++ lib/TimeCard.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
  * User: makoto
  * Date: 2013/10/22
- * Time: 17:58
- * To change this template use File | Settings | File Templates.
  */
 
 namespace SimpleTimeCard;
 
 
-class TimeCard {
-
+class TimeCard
+{
+    protected function init()
+    {}
 }
