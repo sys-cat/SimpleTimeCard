8bae545d823557c3016dca184be41a4375a8a966
makoto sasaki <systemcat91@gmail.com>
2013-10-27 00:00:36 +0900

    create \SimpleTimeCard\TimeCard\setThisMonth()

--- lib/TimeCard.php
+++ lib/TimeCard.php
@@ -25,15 +25,28 @@ class TimeCard
             $this->breakTime = $configArr["breakTime"];
             $this->outTimeLimit = $configArr["outTimeLimit"];
             $this->weekEndFlg = $configArr["weekEnd"];
+            $this->outPutDir = __DIR__.DIRECTORY_SEPARATOR.$configArr["fileSetting"]["outputDir"];
             return true;
         }
     }
 
     // Get this Month
-    // date("Y-m-01");date("Y-m-t");
     public function setThisMonth()
-    {}
+    {
+        $thisYear = date("Y");
+        $thisMonth = date("m");
+        $end = date("t");
+        $days[$thisYear][$thisMonth]=[];
+        $thisDate=[];
+        for($i=1;$i<(int)$end+1;$i++) {
+            $thisDate[$i] = $i;
+        }
+        $days[$thisYear][$thisMonth]=$thisDate;
+        return $days;
+    }
 }
 
 $card = new TimeCard();
+$cal = $card->setThisMonth();
+var_dump($cal);
 var_dump($card);
